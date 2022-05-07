3e01335aadd11e44096e1963ca2a56aa7a5aca09
aryanjais <aryan.jaiswal_cs19@gla.ac.in>
2022-05-07 11:04:37 +0530

    Working

---
+++ client/src/components/ChannelContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+ const ChannelContainer = () =>{
+     return(
+         <div>
+             ChannelContainer
+         </div>
+     )
+ }
+
+ export default ChannelContainer;
